5412ef9714ee88666ca6b7d423ccf884193cf60c
astrawebbiz <astra.web.biz@gmail.com>
2024-07-13 20:47:03 +0300

    First task

--- 1.js
+++ 1.js
@@ -1,1 +1,11 @@
-alert('hellow world');
+/*In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.*/
+function highAndLow(numbers){
+  let array = numbers.split(' ');
+  let min = array[0];
+  let max = array[0];
+  array.forEach((number) => {
+    if (Number(number) < min) min = Number(number);
+    if (Number(number) > max) max = Number(number);
+  });
+  return max.toString() + ' ' + min.toString();
+}
